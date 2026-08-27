f3e22518634412fff5e3a99d66ca9128ae851a46
Pedro Moura <pedro030707@gmail.com>
2026-08-26 22:07:15 -0300

    feat: Adicionando pagina de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Login from "../src/Pages/Pages";
 import "./style.css";
 
 function App() {
+  const [logado, setLogado] = useState(false);
   const [telaAtiva, setTelaAtiva] = useState("camera");
+
+  if (!logado) {
+    return (
+      <div className="celular">
+        <Login aoLogar={() => setLogado(true)} />
+      </div>
+    );
+  }
 
   return (
     <div className="celular">

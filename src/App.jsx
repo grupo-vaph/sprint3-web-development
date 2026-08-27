f8661fd6f5ba3ddf7834a82bbba75bf690c787b0
Pedro Moura <pedro030707@gmail.com>
2026-08-26 21:50:05 -0300

    feat: criando componentes Header e Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./style.css";
 
-const App = () => {
+function App() {
+  const [telaAtiva, setTelaAtiva] = useState("camera");
+
   return (
-    <div>
-      
+    <div className="celular">
+      <Header titulo="Jovi Camera" aoVoltar={() => alert("Voltar clicado")} />
+
+      <main className="conteudo">
+        <p>Tela atual: {telaAtiva}</p>
+      </main>
+
+      <Footer telaAtiva={telaAtiva} aoNavegar={setTelaAtiva} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
